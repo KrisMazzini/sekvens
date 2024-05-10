c3bdd282584ff369a8420c98cf09d740f625021e
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-05-10 14:53:15 -0300

    test(new-player): add player and onRemovePlayer mocks, implement test to validate the correct use of onRemovePlayer

--- src/components/NewPlayer/__tests__/newPlayer.test.tsx
+++ src/components/NewPlayer/__tests__/newPlayer.test.tsx
@@ -1,27 +1,72 @@
-import { render, screen } from '@testing-library/react'
-import { NewPlayer } from '@/components/NewPlayer'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { NewPlayer, Player } from '@/components/NewPlayer'
+
+const player: Player = {
+  name: 'John Doe',
+  id: '1',
+}
 
 describe('Component: NewPlayer', () => {
   it('should render the correct label based on the index prop', () => {
     const index = 0
 
-    render(<NewPlayer index={index} amountOfPlayers={3} />)
+    render(
+      <NewPlayer
+        player={player}
+        index={index}
+        amountOfPlayers={3}
+        onRemovePlayer={() => <></>}
+      />,
+    )
 
     const label = screen.getByText(`Jogador ${index + 1}`)
     expect(label).toBeTruthy()
   })
 
   it("should render the 'Button' component when the amountOfPlayers prop is greater than 1", () => {
-    render(<NewPlayer index={0} amountOfPlayers={2} />)
+    render(
+      <NewPlayer
+        player={player}
+        index={0}
+        amountOfPlayers={2}
+        onRemovePlayer={() => <></>}
+      />,
+    )
 
     const button = screen.getByRole('button')
     expect(button).toBeTruthy()
   })
 
   it("should not render the 'Button' component when the amountOfPlayers prop is equal to 1", () => {
-    render(<NewPlayer index={0} amountOfPlayers={1} />)
+    render(
+      <NewPlayer
+        player={player}
+        index={0}
+        amountOfPlayers={1}
+        onRemovePlayer={() => <></>}
+      />,
+    )
 
     const button = screen.queryByRole('button')
     expect(button).toBeNull()
+  })
+
+  it('should remove the correct player based on the player prop', () => {
+    const handleRemovePlayer = jest.fn()
+
+    render(
+      <NewPlayer
+        player={player}
+        index={0}
+        amountOfPlayers={2}
+        onRemovePlayer={handleRemovePlayer}
+      />,
+    )
+
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+
+    expect(handleRemovePlayer).toHaveBeenCalledTimes(1)
+    expect(handleRemovePlayer).toHaveBeenCalledWith(player.id)
   })
 })

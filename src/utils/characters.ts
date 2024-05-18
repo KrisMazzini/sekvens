2d1ea76149113effbd1e2a196caf614215ce8f4f
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-05-17 23:30:01 -0300

    feat(characters): add nba and nba players characters theme

--- src/utils/characters.ts
+++ src/utils/characters.ts
@@ -8,6 +8,14 @@ import kakashi from '@/assets/characters/naruto/kakashi.png'
 import naruto from '@/assets/characters/naruto/naruto.png'
 import sakura from '@/assets/characters/naruto/sakura.png'
 import sasuke from '@/assets/characters/naruto/sasuke.png'
+import bucks from '@/assets/characters/nba/bucks.png'
+import bulls from '@/assets/characters/nba/bulls.png'
+import lakers from '@/assets/characters/nba/lakers.png'
+import warriors from '@/assets/characters/nba/warriors.png'
+import antetokounmpo from '@/assets/characters/nbaPlayers/antetokounmpo.png'
+import curry from '@/assets/characters/nbaPlayers/curry.png'
+import jordan from '@/assets/characters/nbaPlayers/jordan.png'
+import lebron from '@/assets/characters/nbaPlayers/lebron.png'
 import bulbasaur from '@/assets/characters/pokemon/bulbasaur.png'
 import charmander from '@/assets/characters/pokemon/charmander.png'
 import pikachu from '@/assets/characters/pokemon/pikachu.png'
@@ -54,6 +62,44 @@ export const characters = {
     Red: {
       name: 'Sakura',
       image: sakura,
+    },
+  },
+
+  Nba: {
+    Yellow: {
+      name: 'Lakers',
+      image: lakers,
+    },
+    Green: {
+      name: 'Bucks',
+      image: bucks,
+    },
+    Blue: {
+      name: 'Warriors',
+      image: warriors,
+    },
+    Red: {
+      name: 'Bulls',
+      image: bulls,
+    },
+  },
+
+  NbaPlayers: {
+    Yellow: {
+      name: 'Jordan',
+      image: jordan,
+    },
+    Green: {
+      name: 'Antetokounmpo',
+      image: antetokounmpo,
+    },
+    Blue: {
+      name: 'Curry',
+      image: curry,
+    },
+    Red: {
+      name: 'Lebron',
+      image: lebron,
     },
   },
 
